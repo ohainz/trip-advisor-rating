1ef11222fc0b6ff01cb182bde7c0455d808cab59
ohainz <124523037+ohainz@users.noreply.github.com>
2023-07-14 13:13:03 +0200

    First solution

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# trip-advisor-rating
-Simple chrome extension to evaluate the stars on the site www.tripadvisor.de
+# Trip Advisor - Evaluate Rating Extension
+
+Simple chrome extension to evaluate the stars on the site [https://www.tripadvisor.de](https://www.tripadvisor.de).
+
+## Licenses
+
+I am not sure if I am allowed to use the Icon from the tripadvisor page. This is only an example how you can write a simple chrome extension.
+
+### Icon
+
+I used the icon from the tripadvisor site, to create the icons for the chrome extension:
+
+[https://www.tripadvisor.de/favicon.ico](https://www.tripadvisor.de/favicon.ico)
---
+++ code/trip-advisor-rating/action.js
@@ -0,0 +1,46 @@
+console.log('Trip Advisor Rating extension - action.js');
+
+function doEvaluateRating() {
+	
+	const numOfRatingTypes = 5;
+	const numOfPositiveRatingTypes = 2;
+
+	console.log('action.js - doEvaluateRating() called');
+	var divReviews = document.getElementById('REVIEWS');
+	if (!divReviews) {
+		alert("Could not found the the reviews on this page");
+		return;
+	}
+	console.log('action.js - reviews element found');
+	divReviews.scrollIntoView();
+	var listOfRatingTypes = document.querySelectorAll('section#REVIEWS div.IMmqe div.biGQs._P.pZUbB.osNWb')
+	if (listOfRatingTypes.length <= 0) {
+		alert("Could not evaluate the reviews (List of rating types not found)");
+		return;
+	}
+	if (listOfRatingTypes.length != numOfRatingTypes) {
+		alert("Could not evaluate the reviews (List of ratings does no contain 5 elements)");
+		return;
+	}
+	console.log('action.js - list of ratings has 5 elements');
+	
+	var countPositive = 0.0;
+	var countAll = 0.0;
+	for (var i = 0; i < numOfRatingTypes; i++) {
+		var count = parseFloat(listOfRatingTypes[i].innerHTML);
+		if (i < numOfPositiveRatingTypes) {
+			countPositive += count;
+		}
+		countAll += count;		
+	}
+	var percentage = countPositive / countAll * 100.0;
+	var message = `Sum of ratings is: ${countAll}\nSum of positive ratings is: ${countPositive}\nPercentage of positive ratings is: ${percentage.toFixed(2)} %`;
+	alert(message);
+}
+
+chrome.action.onClicked.addListener((tab) => {
+    chrome.scripting.executeScript({
+		target: { tabId: tab.id },
+		function: doEvaluateRating
+	});
+});
